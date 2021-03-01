75f629857b72fd821a22ddfd52bf1fe7b750dd2b
ferror <zmalcherczyk@gmail.com>
2021-03-01 20:52:46 +0100

    Create events table migration

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: heroku-php-nginx -C nginx_app.conf public/
+release: bin/console doctrine:migrations:migrate
---
+++ migrations/Version20210222193350.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20210222193350 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $sql = <<<SQL
+CREATE TABLE IF NOT EXISTS events (
+    id INT(11) AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    user_id INT(11) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+)
+SQL;
+        $this->connection->executeStatement($sql);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $sql = <<<SQL
+DROP TABLE IF EXISTS events;
+SQL;
+        $this->connection->executeStatement($sql);
+    }
+}
